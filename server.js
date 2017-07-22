062d1f823c60e1c28c0d3f89574f829d85162bb5
TheDen <deniskhoshaba@gmail.com>
2017-07-22 16:25:43 +1000

    update server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-var liveServer = require("live-server");
+var express = require('express')
+var http = require('http')
+var path = require('path')
+var reload = require('reload')
+var bodyParser = require('body-parser')
+var logger = require('morgan')
+var watch = require('watch')
+ 
+var app = express()
+ 
+var publicDir = path.join(__dirname, 'public')
+ 
+app.set('port', process.env.PORT || 3000)
+app.use(logger('dev'))
+app.use(bodyParser.json()) // Parses json, multi-part (file), url-encoded 
+ 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(publicDir, 'htop.html'))
+})
+ 
+var server = http.createServer(app)
+ 
+reloadServer = reload(app);
+watch.watchTree(__dirname + "/public", function (f, curr, prev) {
+    // Fire server-side reload event
+    reloadServer.reload();
+});
 
-var params = {
-	port: 8181, // Set the server port. Defaults to 8080.
-	host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-	root: "./htop.html", // Set root directory that's being served. Defaults to cwd.
-	open: false, // When false, it won't load your browser by default.
-	ignore: 'scss,my/templates', // comma-separated string for paths to ignore
-	file: "index.html", // When set, serve this file for every 404 (useful for single-page applications)
-	wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
-	mount: [['/components', './node_modules']], // Mount a directory to a route.
-	logLevel: 0, // 0 = errors only, 1 = some, 2 = lots
-	middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
-};
-liveServer.start(params);
+server.listen(app.get('port'), function () {
+  console.log('Web server listening on port ' + app.get('port'))
+})
