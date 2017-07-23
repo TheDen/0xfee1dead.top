4bc29b47c761e81a746a58c664382277aa74bd6c
TheDen <deniskhoshaba@gmail.com>
2017-07-24 01:20:57 +1000

    add force flag for mv

--- htopgen.sh
+++ htopgen.sh
@@ -5,6 +5,6 @@ echo q | htop | aha --stylesheet --black --line-fix | \
     sed 's/<title>stdin/<title>\/usr\/bin\/htop/g' | \
     sed 's/<\/body>/<script src="\/reload.js"><\/script><\/body>/g' | \
   sed "s/<head>/<head><link rel='shortcut icon' href='favicon.ico' type='image\/x-icon' \/>/g" \
-    > ./public/htop.html.aux && mv ./public/htop.html.aux ./public/htop.html; \
+    > ./public/htop.html.aux && mv -f ./public/htop.html.aux ./public/htop.html; \
     sleep 2; 
 done
