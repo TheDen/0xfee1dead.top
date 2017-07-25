efe004dd44f2325cbf96bf823c4524ff58d0ee74
TheDen <deniskhoshaba@gmail.com>
2017-07-25 22:23:04 +1000

    update to socket.io

--- server.js
+++ server.js
@@ -1,32 +1,46 @@
-var express = require('express')
-var http = require('http')
-var path = require('path')
-var reload = require('reload')
-var bodyParser = require('body-parser')
-var logger = require('morgan')
-var watch = require('watch')
-
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
 var app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var watch = require('watch');
 
-var publicDir = path.join(__dirname, 'public')
 app.use(express.static('public'))
+var publicDir = path.join(__dirname, 'public')
 
 
-app.set('port', process.env.PORT || 8080)
-app.use(logger('dev'))
-app.use(bodyParser.json()) // Parses json, multi-part (file), url-encoded
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(publicDir, 'htop.html'))
-})
-
-var server = http.createServer(app)
-
-reloadServer = reload(app);
-watch.watchTree(__dirname + "/public", function (f, curr, prev) {
-    reloadServer.reload();
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(app.get('port'), function () {
-  console.log('Web server listening on port ' + app.get('port'))
-})
+sockets = [];
+io.on('connection', function(socket){
+//  console.log('a user connected');
+  sockets.push(socket);
+
+  io.on('disconnection', () => {
+    index = sockets.indexOf(socket);
+    socket.splice(index, 1);
+//    console.log('disconnected');
+  })
+});
+
+port = 8080 
+http.listen(port, function(){
+  console.log('listening on ' + port);
+});
+
+watch.watchTree(__dirname + '/public', function (f, curr, prev) {
+  if (typeof f === 'string' && f.endsWith('htop.html')) {
+    fs.readFile(__dirname + '/public/htop.html', 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      sockets.forEach(s => {
+          s.emit('fileUpdate', data);
+      })
+    });
+  }
+});
