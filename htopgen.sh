0d9c616f66582e12eced860e9dc4e092848553ad
Christopher Dudas <kristof.dudas@gmail.com>
2017-07-26 10:04:54 +1000

    Update htopgen.sh

--- htopgen.sh
+++ htopgen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 touch public/htop.html
-touch publoc/htop.html.aux
+touch public/htop.html.aux
 while true; do echo q | htop | aha --stylesheet --black --line-fix | sed 's/<\/style>/body {overflow-x: hidden; font-size: 2vh;}<\/style>/g' > ./public/htop.html.aux && mv -f ./public/htop.html.aux ./public/htop.html ; sleep 2; done
 #while true; do
 #}echo q | htop | aha --stylesheet --black --line-fix \
