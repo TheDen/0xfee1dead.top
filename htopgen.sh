4951db6466f7c9957a6af4c78738ae916ff67195
TheDen <deniskhoshaba@gmail.com>
2019-10-15 00:51:45 +1100

    shfmt

--- htopgen.sh
+++ htopgen.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
 touch public/htop.html
 touch public/htop.html.aux
-while true; do echo q | htop | aha --stylesheet --black --line-fix | sed 's/<\/style>/body {overflow-x: hidden; font-size: 2vh;}<\/style>/g' > ./public/htop.html.aux && mv -f ./public/htop.html.aux ./public/htop.html ; sleep 2; done
+
+while true; do
+  echo q | htop | aha --stylesheet --black --line-fix | sed 's/<\/style>/body {overflow-x: hidden; font-size: 2vh;}<\/style>/g' > ./public/htop.html.aux && mv -f ./public/htop.html.aux ./public/htop.html
+  sleep 2
+done
